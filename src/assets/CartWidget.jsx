af1a75d22fd2d77a081c9ad4c78043960a3cb537
Juanm0 <juanm.navarro05@gmail.com>
2025-10-21 15:52:48 -0300

    cambiamos el favicon por el de react

--- src/assets/CartWidget.jsx
+++ src/assets/CartWidget.jsx
@@ -2,7 +2,7 @@ function CartWidget() {
   return (
     <div style={{ fontSize: "1.5rem", display: "flex", alignItems: "center", color: "#333" }}>
       🛒
-      <span style={{ marginLeft: "5px", fontSize: "1rem" }}>3</span>
+      <span style={{ marginLeft: "5px", fontSize: "1rem" }}>0</span>
     </div>
   );
 }
